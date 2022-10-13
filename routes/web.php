7261e3b004faab9f9e10120152316ce01d7efe2f
Angel <angelolguin94@hotmail.com>
2022-10-13 09:50:20 -0600

    actividad de hoy xd

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('saludo/{name}', function ($name) {
+    echo 'welcome '.$name;
+});
+
+Route::get('saludo/{num1}/{num2}/{num3?}', function ($num1, $num2, $num3=0) {
+    echo "suma: ".$num1 + $num2 + $num3;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+', 'num3' => '[0-9]+']);
+
+Route::post('suma/', function(Request $request){
+
+});
