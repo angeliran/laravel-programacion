b6d88032f32e9dd9ab3463443caf77b194c5d0f2
Angel <angelolguin94@hotmail.com>
2022-10-26 15:43:50 -0600

    cambios

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Reservation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +14,13 @@ class ReservationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $reservation = new Reservation();
+
+        $reservation->client_id = "1";
+        $reservation->arrival_date = "2010-12-01";
+        $reservation->departure_date = "2010-12-03";
+        $reservation->room_id = "1";
+        
+        $reservation->save();
     }
 }
